4e6abacf597660975b28cd30cf626c1edc10276c
thirunar <thirunar@thoughtworks.com>
2015-09-15 01:55:24 +0530

    Sample with Soomla

---
+++ Assets/Login.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using Soomla.Profile;
+
+public class Login : MonoBehaviour{
+
+	void Start() 
+	{
+
+	}
+	public void LoginFB() 
+	{
+		SoomlaProfile.Login (Provider.FACEBOOK, "", null);
+	}
+	
+
+}
